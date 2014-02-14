2ed64982b83099921f0d559decb796fbe1ae26e5
Shengyun Zhou <Jason@Shengyuns-MacBook-Pro.local>
2014-02-13 16:49:07 -0800

    fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Warmup::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'user#client'
+  root 'user#index'
   get '/users/login', to: 'user#login'
   get '/users/add', to: 'user#add'
   # Example of regular route:
