a8b9a8fe39ddf52a90f70ed497f6f54fbcd92497
szhou8813 <zealot.x@hotmail.com>
2014-02-20 10:10:38 -0800

    should be working now

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,7 @@ class UserController < ApplicationController
         @user = User.new(name: name, password: password)
         @user.count = 1
         if @user.save then
-            render json: {errCode: User.SUCCESS, count:1}
+            render json: {errCode: User.success.to_s, count: 1.to_s}
         else
             message = @user.errors.values[0]
             render json: {errCode: message}
@@ -16,12 +16,13 @@ class UserController < ApplicationController
     def login
         name = request["user"]
         password = request["password"]
-        @user = User.where(name: name, password: password)
+        @user = User.where(name: name, password: password).first
         unless @user then
-            render json: {errCode: User.RR_BAD_CREDENTIALS} 
+            render json: {errCode: User.err_bad_credentials.to_s} 
         else
-            @user.update(count: @user.count + 1 )
-            render json: {errCode: User.SUCCESS, count: @user.count}
+            @user.count = @user.count + 1
+            @user.save!
+            render json: {errCode: User.success.to_s, count: @user.count.to_s}
         end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,43 @@
 class User < ActiveRecord::Base
-    SUCCESS = 1
-    RR_BAD_CREDENTIALS = -1
-    ERR_USER_EXISTS = -2
-    ERR_BAD_USERNAME = -3
-    ERR_BAD_PASSWORD = -4
+    @@SUCCESS = 1
+    @@ERR_BAD_CREDENTIALS = -1
+    @@ERR_USER_EXISTS = -2
+    @@ERR_BAD_USERNAME = -3
+    @@ERR_BAD_PASSWORD = -4
 
     validates :name, length: {
         minimum: 1,
         maximum: 128,
-        message: ERR_BAD_USERNAME
+        message: @@ERR_BAD_USERNAME.to_s
     }
 
     validates :password, length: {
         minimum: 1,
         maximum: 128,
-        message: ERR_BAD_PASSWORD
+        message: @@ERR_BAD_PASSWORD.to_s
     }
 
     validates :name, uniqueness: {
-        message: ERR_USER_EXISTS
+        message: @@ERR_USER_EXISTS.to_s
     }
 
+    def self.success
+        return @@SUCCESS
+    end
+
+    def self.err_bad_credentials
+        return @@ERR_BAD_CREDENTIALS
+    end
+    
+    def self.err_user_exists
+        return @@ERR_USER_EXISTS
+    end
+    
+    def self.err_bad_username
+        return @@ERR_BAD_USERNAME
+    end
+    
+    def self.err_bad_password
+        return @@ERR_BAD_PASSWORD
+    end
 end
